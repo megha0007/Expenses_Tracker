e3ee1de0b2acc50d88106da49a2b81b8a1a20e3e
megha0007 <110972176+megha0007@users.noreply.github.com>
2025-07-29 17:17:12 +0530

    Add files via upload

---
+++ expense_tracker/expenses/urls.py
@@ -0,0 +1,11 @@
+# expenses/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.expense_list, name='expense_list'),
+    path('add/', views.add_expense, name='add_expense'),
+    path('edit/<int:pk>/', views.edit_expense, name='edit_expense'),
+    path('delete/<int:pk>/', views.delete_expense, name='delete_expense'),
+    path('chart/', views.category_summary_chart, name='category_summary_chart'),
+]
